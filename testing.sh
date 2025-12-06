69af9be856f743232026479e953665de2ad76a43
Eric-CyberPatriot <ericchenschool0@gmail.com>
2025-12-06 16:38:04 -0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -420,7 +420,7 @@ passwd -l root || true
 
 # Login definitions
 sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS 90/' /etc/login.defs
-sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 7/' /etc/login.defs
+sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 2/' /etc/login.defs
 sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE 14/' /etc/login.defs
 
 # PWQuality
